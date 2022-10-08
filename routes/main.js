fc80133bdc1eae5fdb33e53065b7630e41bc0808
aselole josjos <corvussmanichi@gmail.com>
2022-10-08 20:45:34 +0800

    asemalaka

--- routes/main.js
+++ routes/main.js
@@ -4,11 +4,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.sendFile(__path + '/views/index.html')
+    res.sendFile(__path + '/views/home.html')
 })
 
 router.get('/home', (req, res) => {
-    res.sendFile(__path + '/views/home.html')
+    res.sendFile(__path + '/views/index.html')
 })
 
 router.get('/apis', (req, res) => {
